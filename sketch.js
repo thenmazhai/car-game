b550cdd4f9b9d051ea74727c7b1dc94487481bfa
thenmazhai <84840022+thenmazhai@users.noreply.github.com>
2021-07-15 01:17:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas, bgimg;
+var gameState=0;
+var playerCount;
+var database;
+var form, player,game;
+
+function setup(){
+   createCanvas(400,400);
+   database = firebase.database();
+   game=new Game();
+   game.getState();
+   game.start();
+
+  
+}
+
+function draw(){
+ 
+  
+
+  
+}
+
